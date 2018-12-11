aedc445f833fdaef46d45617ddfe3ad843ad8de8
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-12-11 07:50:36 -0500

    Add version information

--- pkg/brew/uli-bin.rb
+++ pkg/brew/uli-bin.rb
@@ -1,8 +1,8 @@
 class ULI < Formula
   desc "Universal Loan Identifier (ULI) validation and check digit generation for the Home Mortgage Disclosure Act (HMDA)"
-  version '0.2.0'
-  url ""
-  sha256 ""
+  version 'v0.2.0'
+  url "https://github.com/jmarin/uli/archive/#{version}.tar.gz"
+  sha256 "51bd372617fdc159639181b508e04fd7aaea244e"
   head "https://github.com/jmarin/uli.git"
   depends_on "rust" => :build
 
